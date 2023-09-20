a012af492413ed2d4947d144b768810610e7f469
newton mathias <newtymathias@gmail.com>
2023-09-20 23:09:29 +0300

    crud

---
+++ app/(dashboard)/_components/sidebar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Logo } from "./logo";
+import { SidebarRoutes } from "./sidebar-routes";
+
+const SideBar = () => {
+  return (
+    <div className="h-full border-r flex flex-col overflow-auto bg-white shadow-sm">
+      <div className="p-6">
+        <Logo />
+      </div>
+      <div className="flex flex-col w-full">
+        <SidebarRoutes />
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
